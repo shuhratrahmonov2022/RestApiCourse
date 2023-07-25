04c8b211d80c93566689f4345466646d7f3e24ad
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-07-25 14:38:56 +0500

    INFRA: Initialize WebApi Projects

---
+++ Movies.Contracts/Responses/MoviesResponses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Responses
+{
+    public class MoviesResponses
+    {
+        public required IEnumerable<MovieResponse> Item { get; init; } = Enumerable.Empty<MovieResponse>();
+    }
+}
